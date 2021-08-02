e22612d3774afc9fd73a26f5a8a68ad0d75544ef
taq368 <84571379+taq368@users.noreply.github.com>
2021-08-02 08:51:01 +0300

    Spring
    Spring

---
+++ src/main/java/App.java
@@ -0,0 +1,25 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1.getMessage());
+
+        Cat beanCat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat2.getMessage());
+        System.out.println(bean == bean2);
+        System.out.println(beanCat1 == beanCat2);
+    }
+}
